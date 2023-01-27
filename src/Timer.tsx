9625b9e122e7891986b0e4defec4fce024c94771
Jun <wong.junwei@petronas.com>
2023-01-27 17:38:18 +0800

    refactor(UI): timer with constants value

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,9 +1,17 @@
 import { Button, Container, Typography, Box } from '@mui/material';
 import { useState } from 'react';
 import { useTimer } from 'react-timer-hook';
+import {
+  POMODORO,
+  SHORT_BREAK_DURATION,
+  SHORT_BREAK,
+  LONG_BREAK_DURATION,
+  LONG_BREAK,
+  POMODORO_DURATION,
+} from './constants';
 
 function Timer() {
-  const [currentSeconds, setCurrentSeconds] = useState(1500);
+  const [currentSeconds, setCurrentSeconds] = useState(POMODORO_DURATION);
   const time = new Date();
   time.setSeconds(time.getSeconds() + currentSeconds); // 10 minutes timer
   const { minutes, seconds, start, resume, restart, pause, isRunning } =
@@ -12,39 +20,44 @@ function Timer() {
       autoStart: false,
       onExpire: () => {},
     });
+
   const resetTimeHandler = (resetSeconds: number) => {
     setCurrentSeconds(resetSeconds);
     const resetTime = new Date();
     resetTime.setSeconds(resetTime.getSeconds() + resetSeconds);
     restart(resetTime);
   };
+
+  const getButtonVariant = (secondsValue: number) => {
+    if (secondsValue === currentSeconds) return 'outlined';
+    return 'text';
+  };
+
   return (
     <Container sx={{ height: '80vh' }}>
-      {/* <Typography variant="h3" align="center">
-          Timer
-        </Typography> */}
       <Box textAlign="center" sx={{ py: '10px' }}>
         <Button
-          variant={currentSeconds === 1500 ? 'outlined' : 'text'}
-          onClick={() => resetTimeHandler(1500)}
+          variant={getButtonVariant(POMODORO_DURATION)}
+          onClick={() => resetTimeHandler(POMODORO_DURATION)}
         >
-          Pomodoro
+          {POMODORO}
         </Button>
         <Button
-          variant={currentSeconds === 300 ? 'outlined' : 'text'}
-          onClick={() => resetTimeHandler(300)}
+          variant={getButtonVariant(SHORT_BREAK_DURATION)}
+          onClick={() => resetTimeHandler(SHORT_BREAK_DURATION)}
         >
-          Short Break
+          {SHORT_BREAK}
         </Button>
         <Button
-          variant={currentSeconds === 600 ? 'outlined' : 'text'}
-          onClick={() => resetTimeHandler(600)}
+          variant={getButtonVariant(LONG_BREAK_DURATION)}
+          onClick={() => resetTimeHandler(LONG_BREAK_DURATION)}
         >
-          Long Break
+          {LONG_BREAK}
         </Button>
       </Box>
       <Typography variant="h2" textAlign="center">
-        {minutes}:{seconds}
+        {minutes < 10 ? `0${minutes}` : minutes}:
+        {seconds < 10 ? `0${seconds}` : seconds}
       </Typography>
       <Box textAlign="center">
         <Button
